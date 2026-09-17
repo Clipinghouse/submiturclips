74fb99c083e1f9bd9a8d2cfa945aa6208db01384
parbat <growteensalesteam@gmail.com>
2026-09-17 16:57:27 +0545

    Add Google Tag Manager scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,23 @@ export default function RootLayout({
       lang="en"
       className={`${poppins.variable} ${poorStory.variable} ${anton.variable} h-full antialiased font-sans`}
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JCB3T29');`,
+          }}
+        />
+      </head>
       <body className="min-h-full bg-zinc-950 flex flex-col items-center">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JCB3T29" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          }}
+        />
         <GoogleAnalytics />
         <CookieConsent />
         <div className="w-full max-w-md min-h-screen bg-[var(--color-background)] border-x border-zinc-800 shadow-2xl flex flex-col">
